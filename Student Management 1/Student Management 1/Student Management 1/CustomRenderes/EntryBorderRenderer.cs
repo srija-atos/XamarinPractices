a1f8d2abb6fe9d6de6f178e610eae43177c034d8
srija-atos <96622241+srija-atos@users.noreply.github.com>
2022-02-02 11:47:33 +0530

    Add files via upload

---
+++ Student Management 1/Student Management 1/Student Management 1/CustomRenderes/EntryBorderRenderer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Student_Management_1.CustomRenderes
+{
+    public class EntryBorderRenderer:Entry
+    {
+    }
+}
