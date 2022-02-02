e08c1054d3963f3104c82d07c250aadc5b4689f3
srija-atos <96622241+srija-atos@users.noreply.github.com>
2022-02-02 11:50:53 +0530

    Add files via upload

---
+++ Student Management 1/Student Management 1/Student Management 1.Android/CustomRenderer/DroidEntryBorderRenderer.cs
@@ -0,0 +1,38 @@
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+
+
+using Student_Management_1.CustomRenderes;
+using Student_Management_1.Droid.CustomRenderer;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(EntryBorderRenderer), typeof(DroidEntryBorderRenderer))]
+namespace Student_Management_1.Droid.CustomRenderer
+{
+    public class DroidEntryBorderRenderer : EntryRenderer
+    {
+        public DroidEntryBorderRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                var nativeEditText = (Android.Widget.EditText)Control;
+                float[] arr1 = { 40, 40, 40, 40, 40, 40, 40, 40 };
+                float[] arr2 = { 40, 40, 40, 40, 40, 40, 40, 40 };
+                var shape = new ShapeDrawable(new Android.Graphics.Drawables.Shapes.RoundRectShape(arr1, null, arr2));
+                shape.Paint.Color = Xamarin.Forms.Color.White.ToAndroid();
+                shape.Paint.SetStyle(Paint.Style.Stroke);
+                shape.Paint.StrokeWidth = 4;
+                nativeEditText.Background = shape;
+            }
+        }
+    }
+}
---
+++ Student Management 1/Student Management 1/Student Management 1.Android/CustomRenderer/StudentEntryBorderRenderer.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Student_Management_1.CustomRenders;
+using Student_Management_1.Droid.CustomRenderer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(EntryBorderRender),typeof(StudentEntryBorderRenderer))]
+namespace Student_Management_1.Droid.CustomRenderer
+{
+    public class StudentEntryBorderRenderer : EntryRenderer
+    {
+      
+        public StudentEntryBorderRenderer(Context context) : base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if(e.NewElement != null)
+            {
+                var nativeEditText = (Android.Widget.EditText)Control;
+                var shape = new ShapeDrawable(new Android.Graphics.Drawables.Shapes.RectShape());
+                shape.Paint.Color = Xamarin.Forms.Color.White.ToAndroid();
+                shape.Paint.SetStyle(Paint.Style.Stroke);
+                shape.Paint.StrokeWidth = 4;
+                nativeEditText.Background = shape;
+            }
+        }
+    }
+}
