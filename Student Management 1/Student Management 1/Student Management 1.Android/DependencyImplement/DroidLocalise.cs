c053dd6eaa37cc5b02f78077a725aeb6a9559d49
srija-atos <96622241+srija-atos@users.noreply.github.com>
2022-02-02 11:51:24 +0530

    Add files via upload

---
+++ Student Management 1/Student Management 1/Student Management 1.Android/DependencyImplement/DroidLocalise.cs
@@ -0,0 +1,93 @@
+﻿using Student_Management_1.DependencyInterfaces;
+using Student_Management_1.Droid.DependencyInterfaces;
+using System.Globalization;
+using System.Threading;
+
+[assembly: Xamarin.Forms.Dependency(typeof(Student_Management_1.Droid.DependencyImplement.DroidLocalise))]
+namespace Student_Management_1.Droid.DependencyImplement
+{
+    public class DroidLocalise : ILocalize
+    {
+        public void SetLocale(CultureInfo ci)
+        {
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+        }
+
+        public CultureInfo GetCurrentCultureInfo()
+        {
+            var androidLocale = Java.Util.Locale.Default;
+            string netLanguage = AndroidToDotnetLanguage(androidLocale.ToString().Replace("_", "-"));
+
+            // this gets called a lot - try/catch can be expensive so consider caching or something
+            CultureInfo ci;
+            try
+            {
+                ci = new CultureInfo(netLanguage);
+            }
+            catch
+            {
+                // iOS locale not valid .NET culture (eg. "en-ES" : English in Spain)
+                // fallback to first characters, in this case "en"
+                try
+                {
+                    var fallback = ToDotnetFallbackLanguage(new PlatformCulture(netLanguage));
+                    ci = new CultureInfo(fallback);
+                }
+                catch
+                {
+                    // iOS language not valid .NET culture, falling back to English
+                    ci = new CultureInfo("en");
+                }
+            }
+
+            return ci;
+        }
+
+        private string AndroidToDotnetLanguage(string androidLanguage)
+        {
+            var netLanguage = androidLanguage;
+
+            //certain languages need to be converted to CultureInfo equivalent
+            switch (androidLanguage)
+            {
+                case "ms-BN":   // "Malaysian (Brunei)" not supported .NET culture
+                case "ms-MY":   // "Malaysian (Malaysia)" not supported .NET culture
+                case "ms-SG":   // "Malaysian (Singapore)" not supported .NET culture
+                    netLanguage = "ms"; // closest supported
+                    break;
+                case "in-ID":  // "Indonesian (Indonesia)" has different code in  .NET 
+                    netLanguage = "id-ID"; // correct code for .NET
+                    break;
+                case "gsw-CH":  // "Schwiizertüütsch (Swiss German)" not supported .NET culture
+                    netLanguage = "de-CH"; // closest supported
+                    break;
+                    // add more application-specific cases here (if required)
+                    // ONLY use cultures that have been tested and known to work
+            }
+
+            return netLanguage;
+        }
+
+        private string ToDotnetFallbackLanguage(PlatformCulture platformCulture)
+        {
+            var netLanguage = platformCulture.LanguageCode; // use the first part of the identifier (two chars, usually);
+
+            switch (platformCulture.LanguageCode)
+            {
+                case "gsw":
+                    netLanguage = "de-CH"; // equivalent to German (Switzerland) for this app
+                    break;
+                    // add more application-specific cases here (if required)
+                    // ONLY use cultures that have been tested and known to work
+            }
+
+            return netLanguage;
+        }
+
+        public bool Is24HourFormat()
+        {
+            return Android.Text.Format.DateFormat.Is24HourFormat(Android.App.Application.Context);
+        }
+    }
+}
